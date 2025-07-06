f4e702807b1491a520ca661a01258c455027dd23
cn-gourav <mrgusain558@gmail.com>
2025-07-06 22:42:35 +0530

    ADD :: porject 11

---
+++ 09ContextApi02/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+import { ThemeProvider } from './components/theme';
+import ThemeBtn from './template/ThemeBtn';
+import Card from './template/Card';
+
+const App = () => {
+  const [themeMode, setThemeMode] = useState("light");
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darhTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darhTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
